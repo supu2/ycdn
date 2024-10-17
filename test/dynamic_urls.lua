5f18c9fc81a254f273d6588757c1ee015a94ccce
Supu <supu@incloudy.io>
2024-10-17 18:39:59 +0300

    test

--- test/dynamic_urls.lua
+++ test/dynamic_urls.lua
@@ -1,6 +1,6 @@
 -- Define the range for the dynamic URLs
 min = 100
-max = 500  -- Adjust this based on the number of URLs you want to generate
+max = 200  -- Adjust this based on the number of URLs you want to generate
 
 -- Seed the random number generator
 math.randomseed(os.time())
@@ -10,6 +10,6 @@ request = function()
     -- Generate a random URL in the specified range
     local width = math.random(min, max)
     local height = math.random(min, max)
-    local path = "/resize/" .. width .. "/".. height .. "/aHR0cHM6Ly9jZG4uZHNtY2RuLmNvbS90eTk1L3Byb2R1Y3QvbWVkaWEvaW1hZ2VzLzIwMjEwNDA0LzE1LzRkYTFiMTRiLzEzNjIzODAzLzEvMV9vcmdfem9vbS5qcGc"
+    local path = "/resize/" .. width .. "/".. height .. "/aHR0cDovLzE5Mi4xNjguMTIyLjE6ODAwMC8udG1wLzhLLmpwZWc"
     return wrk.format("GET", path)
 end
